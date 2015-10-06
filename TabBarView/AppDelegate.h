9effe808730e4bb7cafd1d3352742153e84edc03
clouddeep <shoujhengduan@gmail.com>
2015-10-06 21:18:19 +0800

    Initial Commit

---
+++ TabBarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBarView
+//
+//  Created by Shou Cheng Tuan  on 2015/10/6.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabBarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarView
+//
+//  Created by Shou Cheng Tuan  on 2015/10/6.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
