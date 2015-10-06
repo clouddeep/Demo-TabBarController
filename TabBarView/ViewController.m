ac6e861140338d84970be6f48a8b539c11f5ce6a
clouddeep <shoujhengduan@gmail.com>
2015-10-07 07:12:36 +0800

    TabBarComtroller Demo
    Simple demo to show how to use TabBarComtroller to manage
    ViewControllers.

--- TabBarView/ViewController.m
+++ TabBarView/ViewController.m
@@ -8,20 +8,83 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)loadView
+{
+    [super loadView];
+    
+    UITabBarController *tabBarController = [[UITabBarController alloc] init];
+    
+    UIViewController *vcRed = [self createTabbedNavigationController:@"red" withTag:0];
+    UIViewController *vcBlue = [self createTabbedNavigationController:@"blue" withTag:1];
+    UIViewController *vcYellow = [self createTabbedNavigationController:@"yellow" withTag:2];
+    tabBarController.viewControllers = @[vcRed, vcBlue, vcYellow];
+    tabBarController.delegate = self;
+    
+    self.tabBarController = tabBarController;
+    [self.view addSubview:tabBarController.view];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.tabBarController.tabBar.hidden = YES;
+    
+    UIToolbar *toolBar = [[UIToolbar alloc] init];
+    CGSize screenSize = self.view.bounds.size;
+    toolBar.frame = CGRectMake(0, screenSize.height - 50, screenSize.width, 50);
+    UIBarButtonItem *itemRed = [self createPlainBarButton:@"red" withTag:0];
+    UIBarButtonItem *itemBlue = [self createPlainBarButton:@"blue" withTag:1];
+    UIBarButtonItem *itemYellow = [self createPlainBarButton:@"yellow" withTag:2];
+    UIBarButtonItem *sizeItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    toolBar.items = @[itemRed, sizeItem, itemBlue, sizeItem, itemYellow];
+    [self.view addSubview:toolBar];
+}
+
+- (UINavigationController *)createTabbedNavigationController:(NSString *)title withTag:(NSInteger)tag
+{
+    UIViewController *vc = [self createDummyViewController:title];
+    vc.tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:nil tag:tag];
+    return [self createNavigationController:vc];
+}
+
+- (UINavigationController *)createNavigationController:(UIViewController *)viewController
+{
+    UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:viewController];
+    return nc;
+}
+
+- (UIViewController *)createDummyViewController:(NSString *)title
+{
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.title = title;
+    
+    return vc;
+}
+
+- (UIBarButtonItem *)createPlainBarButton:(NSString *)title withTag:(NSInteger)tag
+{
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:title style:UIBarButtonItemStylePlain target:self action:@selector(toggleContentView:)];
+    item.tag = tag;
+
+    return item;
+}
+
+- (void)toggleContentView:(id)sender
+{
+    UIBarButtonItem *item = sender;
+    NSInteger tag = item.tag;
+    self.tabBarController.selectedIndex = tag;
+    
 }
 
 @end
